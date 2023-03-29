b8898018ac69af37684d543528fbeb3ec0d85bf7
manuher16 <73207879+manuher16@users.noreply.github.com>
2023-03-29 16:46:12 -0600

    subiendo checkout

--- store/src/pages/cart.js
+++ store/src/pages/cart.js
@@ -1,18 +1,18 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Navbar from "@/components/navbar";
 import Checkout from "@/components/checkout";
+import styles from '@/styles/Home.module.css'
 export default function Cart() {
     return (
-        <amin>
-
-            <Container className="">
+        <Container>
+            <h1>
                 Carrito Compras
-                <Row>
-                    <Col><Checkout /></Col>
-                    <Col md="2"></Col>
-                </Row>
+            </h1>
+            <Row className="d-flex">
+                <Col ></Col>
+                <Col md="3"><Checkout /></Col>
+            </Row>
 
-            </Container>
-        </amin>
+        </Container>
     )
 }
